deb186e99c68f5a131cf2c2dbaea8c3d563bb972
wpohlhau <wpohlhau@villanova.edu>
2025-02-16 17:53:12 -0500

    Implimented initial funtionality

---
+++ src/main/java/frc/robot/subsystems/CoralGroundIntakePivotSubSystem.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class CoralGroundIntakePivotSubSystem extends SubsystemBase {
+  /** Creates a new CoralGroundIntakePivotSubSystem. */
+  private TalonFX pivotMotor;
+
+  public CoralGroundIntakePivotSubSystem() {
+    pivotMotor = new TalonFX(Constants.CoralGroundIntakePivotSubSystem.CAN_ID_INTAKE_PIVOT);
+  }
+
+
+  public void setIntakePivotSpeed(double speed) {
+    pivotMotor.set(speed);
+  }
+  
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ElevatorSubSystem.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.Follower;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class ElevatorSubSystem extends SubsystemBase {
+  /** Creates a new ElevatorSubSystem. */
+  private TalonFX motor1;
+  private TalonFX motor2;
+
+  private final DigitalInput elevatorLimitSwitch = new DigitalInput(Constants.ElevatorSubSystem.INPUT_ID);
+
+
+  public ElevatorSubSystem() {
+    motor1 = new TalonFX(Constants.ElevatorSubSystem.CAN_ID_MOTOR1);
+    motor2 = new TalonFX(Constants.ElevatorSubSystem.CAN_ID_MOTOR2);
+
+    motor2.setControl(new Follower(motor1.getDeviceID(), false));
+  }
+
+  public void setElevatorSpeed(double speed){
+    motor1.set(speed);
+  }
+
+  public boolean getLimitSwitch(){
+    return elevatorLimitSwitch.get();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
