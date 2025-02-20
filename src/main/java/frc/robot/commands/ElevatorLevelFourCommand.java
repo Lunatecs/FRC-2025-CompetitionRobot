6d775fe87b5afea36890588038bd5d558c1c83f3
wpohlhau <wpohlhau@villanova.edu>
2025-02-19 20:52:29 -0500

    Changed value for PID in L4 command

--- src/main/java/frc/robot/commands/ElevatorLevelFourCommand.java
+++ src/main/java/frc/robot/commands/ElevatorLevelFourCommand.java
@@ -19,7 +19,7 @@ public class ElevatorLevelFourCommand extends Command {
     // Use addRequirements() here to declare subsystem dependencies.
     this.elevator = elevator;
     addRequirements(elevator);
-    controller = new PIDController(0.035, 0, 0); //0.0139, 0.017375, 0.019, 0.022, 0.0275, 0.031
+    controller = new PIDController(0.05, 0, 0); //0.0139, 0.017375, 0.019, 0.022, 0.0275, 0.031
     controller.setSetpoint(72.0);
     controller.setTolerance(0.25);
   }
