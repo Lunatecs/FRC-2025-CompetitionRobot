2543e8ba37ad2a6d13754219a0835e6024dca5c2
wpohlhau <wpohlhau@villanova.edu>
2025-03-25 18:18:30 -0400

    Code from competition

--- src/main/java/frc/robot/commands/NewAlageDown.java
+++ src/main/java/frc/robot/commands/NewAlageDown.java
@@ -4,20 +4,27 @@
 
 package frc.robot.commands;
 
+import java.time.Instant;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.AlgaeLiberatorSubSystem;
 import frc.robot.subsystems.AlgaePivotSubSystem;
+import frc.robot.subsystems.CoralOutakeSubSystem;
 import frc.robot.subsystems.ElevatorSubSystem;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class NewAlageDown extends SequentialCommandGroup {
+public class NewAlageDown extends ParallelCommandGroup {
   /** Creates a new NewAlageDown. */
-  public NewAlageDown(AlgaePivotSubSystem pivot, ElevatorSubSystem elevator) {
+  public NewAlageDown(AlgaePivotSubSystem pivot, ElevatorSubSystem elevator, AlgaeLiberatorSubSystem roller, CoralOutakeSubSystem outake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new AlgaePivotResetCommand(pivot),
+      new InstantCommand(()-> {roller.setSpeed(0.04); outake.setSpeed(0);}, roller, outake),
       new ElevatorDownCommand(elevator)
     );
   }
