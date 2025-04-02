d545dd898a80d6989d540da06e5896bd5a56c87e
wpohlhau <wpohlhau@villanova.edu>
2025-04-01 20:34:24 -0400

    Changed degrees, elevator level 4 setpoint, and the amount it shot out for shoot coral command.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
 
   @Override public void driverStationConnected() {
 
-      double targetRotationDegrees = (DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red) ? 180 : 0; //Switched 180 and 0
+      double targetRotationDegrees = (DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red) ? 0 : 180; //Switched 180 and 0, 4/1 SWITCHED IT BACK
       m_robotContainer.drivetrain.resetRotation(Rotation2d.fromDegrees(targetRotationDegrees));
   }
 
